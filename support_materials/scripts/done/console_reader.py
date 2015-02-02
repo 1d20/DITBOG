56c1a554f26ac73fe528765c9aa6b4b8dde16fe8
detonavomek <detonavomek@gmail.com>
2015-02-02 18:58:55 +0200

    change gitignore

--- support_materials/scripts/done/console_reader.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-path_to_script = '/Users/Detonavomek/Documents/Python_projects/Django/ditbog/support_scripts/working/printer.py'
-theme_name = 'theme N'
-command = 'python '+path_to_script+' \''+theme_name+'\''
-
-result = subprocess.check_output(command, shell=True)
-print 'Result: '+ result[:-1]
--- support_materials/scripts/done/printer.py
+++
@@ -1,3 +0,0 @@
-import sys
-theme_name = sys.argv[1]
-print 'ad: '+theme_name
