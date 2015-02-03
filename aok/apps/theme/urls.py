a38e124e4af15c219f1d13eaf0f8f9577eee16f8
POD666 <pod666@i.ua>
2015-02-03 03:55:35 +0200

    editable description description save its changes by itself

--- aok/apps/theme/urls.py
+++ aok/apps/theme/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
+
     url(r'themes/$', views.themes, name='themes'),
     url(r'themes/(?P<engine_id>\d+)/$', views.themes, name='themes-engine'),
     url(r'zip/(?P<type>\w+)/$', views.zip, name='theme-zip-type'),
     url(r'change/$', views.change, name='theme-change'),
     url(r'add/$', views.add, name='theme-add'),
     url(r'show/(?P<theme_id>\d+)/$', views.show, name='theme-show'),
+    
+    url(r'edit_description/(?P<desc_id>\d+)/$', views.edit_description),
 )
